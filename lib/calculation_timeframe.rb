12d83ddf760a0c17ff6645b457f34c54ab2d3213
Thomas Jacobs <JacobsT@businessLogic.com>
2014-06-13 16:45:21 -0500

    Fix quarter timeframes being off by one day

--- lib/calculation_timeframe.rb
+++ lib/calculation_timeframe.rb
@@ -64,7 +64,8 @@ module CalculationTimeframe
 
     def extract_quarter(input)
       year, quarter = input.scan(/[0-9]+/).map(&:to_i)
-      Timeframe.new(:months, 3, 0, Time.new(year) + 3.month * quarter)
+      Timeframe.new(:days, 0, 0, Time.new(year).advance(months: 3 * (quarter - 1)),
+        Time.new(year).advance(months: 3 * quarter, days: -1))
     end
 
     def extract_last_timeframe(input)
